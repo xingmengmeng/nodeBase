9ac90e786373026989eed8ea609cac29d8136aae
zhufengnodejs <zhufengnodejs@126.com>
2016-12-03 17:39:20 +0800

    send

--- 1.express/8.tmpl.js
+++ 1.express/8.tmpl.js
@@ -10,7 +10,7 @@ app.set('views',path.resolve('./views'));
  * 模板引擎用于把静态的模板内容和数据对象进行渲染得到HTML送发送给客户端
  */
 app.get('/',function(req,res){
-  //1 参数是模板的相对路径 2 参数是数据对象
+  //1 参数是模板的相对路径 2 参数是数据对象,在模板中可以得到数据对象的属性
   res.render('user',{users:users});
 });
 
