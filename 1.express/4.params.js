934b6b2f57351f1cc6f9328d016704868f0f5796
zhufengnodejs <zhufengnodejs@126.com>
2016-12-03 12:11:29 +0800

    express

---
+++ 1.express/4.params.js
@@ -0,0 +1,32 @@
+var express = require('express');
+var log = console.log;
+var app = express();
+/**
+ * 1.获取请求方法，请求的路径名，请求的查询字符串对象
+ * 2.请求头
+ */
+app.get('/users',function(req,res){
+    log(req.method);
+    //获取请求路径
+    log(req.path);// = url.parse(req.url).pathname;
+    //获取查询字符串对象
+    log(req.query);//= url.parse(req.url).query
+    //获取请求头
+    log(req.headers);
+    res.end('users');
+});
+//   /users/1?name=zfpx 也就是说路径可以写正则表达式，只要请求的字符串和此正则相匹配，则执行对应的回调函数
+/*app.get(/\/users\/(\d+)$/,function(req,res){
+    var result = /\/users\/(\d+)$/.exec(req.path);
+    console.log(result);
+    var id = result[1];
+    res.end(id);
+})*/
+//指定路径参数
+app.get('/users/:id/:action',function(req,res){
+    console.log(req.params);
+    res.end(req.params.id);
+});
+
+app.listen(9090);
+
--- 1.express/test.js
+++ 1.express/test.js
@@ -1,7 +1,21 @@
+/*
 function add(a){
      return function(b){
         return a+b;
      }
 }
 var newAdd = add(1);
-console.log(newAdd(2));
+console.log(newAdd(2));*/
+//配置的路径
+var config = '/users/:id/:action';
+//客户端请求过来的url
+var    url = '/users/100/view';
+//需要得到这样的一个对象 {id:100,action:'view'}
+var params = {};
+
+
+
+
+
+
+
