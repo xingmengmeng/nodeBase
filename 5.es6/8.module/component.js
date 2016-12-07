9258c57c77d6dd9a605b17212425a5a4e4088bc4
zhufengnodejs <zhufengnodejs@126.com>
2016-12-07 12:54:06 +0800

    cookie

---
+++ 5.es6/8.module/component.js
@@ -0,0 +1,5 @@
+export default  name = 'zfpx';
+var age = 8;
+
+
+
---
+++ 5.es6/8.module/index.js
@@ -0,0 +1,4 @@
+//把导入的对象进行解构赋值，得到name age 两个局部变量
+//import {name,age} from './component';
+import xxx from './component.js';
+console.log(xxx);
