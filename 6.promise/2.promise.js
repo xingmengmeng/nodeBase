ea50f370386be52c703a4ece4e54f30a36fa9dc0
zhufengnodejs <zhufengnodejs@126.com>
2016-12-07 15:50:00 +0800

    cookie

--- 6.promise/2.promise.js
+++ 6.promise/2.promise.js
@@ -4,6 +4,7 @@
  * @returns {Promise}
  */
 var fs = require('fs');
+var Promise = require('./Promise');
 function readFile(filename){
   //创建一个promise实例并返回
   //构造函数需要一个函数参数，此函数参数会立刻执行。里面会开启一个异步任务，如果任务成功了，调用resolve方法使promise实例状态变成完成态，如果任务失败了，调用reject使promise实例状态变成失败态
---
+++ 6.promise/Promise.js
@@ -0,0 +1,20 @@
+function Promise(task){
+    this._status = '初始态';
+    //调用resolve会把状态改变成成功态
+    var resolve = data => {
+        this._status = '成功态';
+        this._success(data);
+    }
+    //调用reject会把状态改变成失败态
+    var reject = error =>{
+        this._status = '失败态';
+        this._fail(error);
+    }
+    task(resolve,reject);
+
+}
+Promise.prototype.then = function(success,fail){
+  this._success = success;
+  this._fail = fail;
+}
+module.exports =  Promise;
