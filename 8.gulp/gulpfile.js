b6d8df5e283eb4b4e210959469a2a8fa1345f737
zhufengnodejs <zhufengnodejs@126.com>
2016-12-08 12:48:32 +0800

    css

--- 8.gulp/gulpfile.js
+++ 8.gulp/gulpfile.js
@@ -1,19 +1,14 @@
 var gulp = require('gulp');
-var $ = require('./gulp-load-plugins')();
+var $ = require('gulp-load-plugins')();
 
-/**
- * 实现js文件处理
- * 1. 多个js文件合并成一个文件
- * 2. 压缩里面的换行空格和参数名
- * 3. 导出
- **/
-gulp.task('js',function(){
- gulp.src('./app/js/*.js')
-     //把多个文件合并成一个文件，并指定合并后的文件名
-     .pipe($.concat('all.js'))
-     .pipe(gulp.dest('./build/js'))
-     //uglify只能压缩js文件，不能压缩css html
-     .pipe($.uglify())
-     .pipe($.rename('all.min.js'))
-     .pipe(gulp.dest('./build/js'));
+gulp.task('css',function(){
+  gulp.src('./app/less/*.less')
+      .pipe($.less())
+      .pipe($.concat('all.css'))
+      .pipe(gulp.dest('./build/css'))
+      .pipe($.cleanCss())
+      .pipe($.rename(function(file){
+        file.basename += '.min';
+      }))
+      .pipe(gulp.dest('./build/css'))
 });
