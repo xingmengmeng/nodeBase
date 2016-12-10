125aec3569c05ae63b856196539a0fc2b8df0d05
zhufengnodejs <zhufengnodejs@126.com>
2016-12-10 12:58:55 +0800

    mongoose

--- 10.mongodb/1.mongodb.js
+++ 10.mongodb/1.mongodb.js
@@ -13,13 +13,54 @@ var PersonSchema = new mongoose.Schema({
 
 //定义操作数据库的模型 它有一些方法可以操作数据库
 var PersonModel = mongoose.model('Person',PersonSchema);
+//增加数据 保存对象
+/*var persons = [];
+for(var i=1;i<=10;i++){
+  persons.push({name:'zfpx'+i,age:i});
+}
+PersonModel.create(persons,function(err,docs){
+    console.log(docs);
+});*/
+//第一个参数是一个对象，存放着要删除的条件
+//  会删除所有匹配到的记录
+//
+/*PersonModel.remove({age:8,name:'zfpx8'},function(err,result){
+    console.log(result);
+    // ok 1 表示操作成功，n 表示删除的数据量，记录
+    // result: { ok: 1, n: 1 },
+});*/
+/**
+ * 1. 更新的条件
+ * 2. 要把数据更新成什么样子 更新后的数据
+ * 3. 更新的时候，如果匹配到的条数多于一条，只会更新第一条
+ * 4. multi表示如果匹配到多条，那么有几条更新几条
+ */
+/*
+PersonModel.update({age:2,name:'zfpx2'},{
+  birthday:new Date()
+},{multi:true},function(err,result){
+    //{ ok: 1, nModified: 0, n: 0 }
+    // ok=1表示更新成功 nModified 被修改的条数  n准备要修改的条数
+  console.log(result);
+});
+*/
+// name=zfpx4或age5 {$or:[{name:'zfpx4'},{age:5}]}
+/*PersonModel.find({$or:[{name:'zfpx4'},{age:5}]},function(err,docs){
+    console.log(docs);
+});*/
 
-PersonModel.create({
-    name:'张三',
-    age:'a',
-    birthday:new Date(),
-    home:'北京'
-},function(err,doc){
-    console.log(err);
+//属性过滤，只返回需要的字段
+//第二个参数 可以写包含的字段或排除的字段 name表示字段名， 1要返回
+//BadValue Projection cannot have a mix of inclusion and exclusion.
+/*PersonModel.find({},{name:0},function(err,docs){
+    console.log(docs);
+});*/
+
+//findOne找到一条后就返回，不再继续查找了
+PersonModel.findOne({_id:'584b866c531c90e19e3f0d8e'},function(err,doc){
+    console.log(doc);
 });
 
+PersonModel.findById('584b866c531c90e19e3f0d8e',function(err,doc){
+    console.log(doc);
+});
