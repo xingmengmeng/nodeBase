b478c3d163ad28784c2ca65015320e3ea5995da3
zhufengnodejs <zhufengnodejs@126.com>
2016-12-13 17:15:50 +0800

    mongoose

--- 11.react/js/12.ajax.js
+++ 11.react/js/12.ajax.js
@@ -1,4 +1,14 @@
+/**
+ * 1. 为input框绑定onChange事件，当改变后向百度的接口发起ajax调用
+ * 2. 向百度接口https://www.baidu.com/su?wd=b&cb=xxx发起调用
+ * 3. 取得返回对象的s属性，是一个关键字字符串数组
+ * 4. 把此数组转成li数组显示在ul里
+ * @type {*}
+ */
 var Suggest = React.createClass({
+    getInitialState(){
+      return {words:[]};//["baidu","bt"]
+    },
     render(){
         return(
             <div className="panel panel-default">
@@ -7,7 +17,11 @@ var Suggest = React.createClass({
                 </div>
                 <div className="panel-body">
                     <ul className="list-group">
-                        <li className="list-group-item">1234567890</li>
+                        {
+                            this.state.words.map(function(item,index){
+                              return   <li key={index} className="list-group-item">{item}</li>
+                            })
+                        }
                     </ul>
                 </div>
             </div>
