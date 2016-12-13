dd104bf261f429a32157b923b7e485c81434e3ff
zhufengnodejs <zhufengnodejs@126.com>
2016-12-13 10:20:44 +0800

    mongoose

--- 11.react/js/1.react.js
+++ 11.react/js/1.react.js
@@ -1,3 +1,9 @@
 /**
- * Created by Administrator on 2016/12/13.
+ * 1. 框架 则意味着我们只要按规定写好就可以，框架会自动调我们的调用
+ * 2. 库 则意味着我们要调用它的代码
+ * react.js window.React
+ * react-dom.js window.ReactDOM
+ *
  */
+//把这个DOM片断放置在app容器的内部
+ReactDOM.render(<h1>hello</h1>,document.getElementById('app'));
