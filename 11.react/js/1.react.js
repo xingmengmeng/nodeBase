f42e004a4b126cb62e3557e4e2088e0ae7a430de
zhufengnodejs <zhufengnodejs@126.com>
2016-12-13 10:39:06 +0800

    mongoose

--- 11.react/js/1.react.js
+++ 11.react/js/1.react.js
@@ -6,4 +6,15 @@
  *
  */
 //把这个DOM片断放置在app容器的内部
-ReactDOM.render(<h1>hello</h1>,document.getElementById('app'));
+function render(domObj,ele) {
+    var h1 = document.createElement(domObj.tagName);
+    h1.innerHTML = domObj.text;
+    ele.appendChild(h1);
+}
+/**
+ * 1. jsx=js+xml混合写法 ，浏览器不能识别,需要转义
+ * 转义成真正的js代码
+ * 2.此处的xml写的是虚拟DOM,真正渲染的时候并不是直接把它转成字符串放在app内部。而是转成一个JS数据结构
+ * 3.在渲染的时候会根据这个数据结构渲染出真正的DOM元素放到容器的内部
+ */
+render({tagName:'h1',text:'hello'},document.getElementById('app'));
