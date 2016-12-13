8a661515313e671bf128eba4b6d862633f93727d
zhufengnodejs <zhufengnodejs@126.com>
2016-12-13 17:31:41 +0800

    mongoose

--- 11.react/js/12.ajax.js
+++ 11.react/js/12.ajax.js
@@ -9,11 +9,29 @@ var Suggest = React.createClass({
     getInitialState(){
       return {words:[]};//["baidu","bt"]
     },
+    handleChange(event){
+        var word = event.target.value;//获取输入框的值
+        //请求的真实URL https://www.baidu.com/su?cb=jQuery_1481621143705&wd=a&_=1481621143706
+        //响应 jQuery_1481621143705({s:["爱奇艺","阿里巴巴批发网","acfun","安居客","阿里巴巴","阿卡丽的神秘商店","阿里云","爱奇艺网","阿里旺旺","安全教育平台"]});
+        $.ajax({
+            url:'https://www.baidu.com/su',
+            method:'GET',
+            //如果发的是get请求，那么data会转成查询字符串追加到url后面
+            data:{wd:word},
+            jsonp:'cb',//指定的是用来向后台传递回调函数方法名的参数名
+            dataType:'jsonp',
+            context:this,//用来绑定success方法中的this指针
+            success(data){
+                console.log(this);
+                this.setState({words:data.s});
+            }
+        });
+    },
     render(){
         return(
             <div className="panel panel-default">
                 <div className="panel-heading">
-                    <input type="text" className="form-control"/>
+                    <input onChange={this.handleChange} type="text" className="form-control"/>
                 </div>
                 <div className="panel-body">
                     <ul className="list-group">
