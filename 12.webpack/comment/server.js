ec3d481480ccc82adc9f3bb7539a4c86109a2d0a
zhufengnodejs <zhufengnodejs@126.com>
2016-12-14 17:24:00 +0800

    绘制主页面

---
+++ 12.webpack/comment/server.js
@@ -0,0 +1,16 @@
+const COMMENTS = 'COMMENTS';
+module.exports = {
+  //增加一条新的留言
+  add(comment){
+
+  },
+  //移除一个留言
+  delete(id){
+
+  },
+  //查询所有留言 如果有，则转成对象数组，如果没有则返回空数组
+  // localStorage只能存放字符串
+  query(){
+
+  }
+}
