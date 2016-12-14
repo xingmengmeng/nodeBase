44265a8118fcb12cb40c31754a8447cf782df38d
zhufengnodejs <zhufengnodejs@126.com>
2016-12-14 11:37:09 +0800

    2

--- 11.react/js/13.comment.js
+++ 11.react/js/13.comment.js
@@ -1,3 +1,11 @@
+/**
+ * 1. 在组件内定义一个状态 {comments:[]},把这个 comments渲染到ul内部
+ * 2. 给button绑定点击事件，事件发生时取得input框的值，修改状态，状态修改后可以重新渲染页面
+ *  this.state.comments.push(xxx);
+ *  this.setState({comments:this.state.comments});
+ * 3.删除功能
+ * @type {*}
+ */
 var Comment = React.createClass({
     render(){
         return (
