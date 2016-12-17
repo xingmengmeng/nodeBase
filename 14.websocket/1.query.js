11c6f9011671840d7123b0795bfbf73b520ed84f
zhufengnodejs <zhufengnodejs@126.com>
2016-12-17 10:17:29 +0800

    轮播图

---
+++ 14.websocket/1.query.js
@@ -0,0 +1,11 @@
+var express = require('express');
+var app = express();
+app.get('/clock',function(req,res){
+    res.setHeader('Access-Control-Allow-Origin','http://localhost:63342');
+    /**
+     * Allow-Methods 跨域请求的方法 GET,POST,DELETE
+     * Allow-Headers 跨域请求的请求头
+     */
+    res.send(new Date().toLocaleString());
+});
+app.listen(8080);
