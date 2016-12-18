a0ee4579ec18e25348f3efdd594cdae6ea21f35f
zhufengnodejs <zhufengnodejs@126.com>
2016-12-18 15:23:07 +0800

    轮播图

---
+++ 17.crawl/README.md
@@ -0,0 +1,13 @@
+## 1.项目步骤
+### 1.1 初始化项目配置文件
+```
+npm init -y
+```
+### 1.2 安装依赖的模块
+```
+npm install async request cheerio express ejs mongoose --
+save
+```
+## 2.实现功能
+### 2.1 读取URL地址得到电影列表数组
+
---
+++ 17.crawl/read.js
@@ -0,0 +1,32 @@
+/**
+ * 读取url地址，得到电影对象数组
+ * @param url 电影列表的url
+ * @param callback
+ */
+var request = require('request');
+var iconv = require('iconv-lite');
+var cheerio = require('cheerio');
+module.exports = function(url,callback){
+  //读取url地址中的响应体
+  request({url,encoding:null},function(err,response,body){
+     //把buffer转成utf8的字符串
+      body = iconv.decode(body,'gbk');
+      //把body放进去转成jquery对象
+      var $ = cheerio.load(body);
+      var items = [];
+      $('.keyword .list-title').each(function(){
+          var $me = $(this);
+          var item = {
+              name:$me.text(),
+              url:$me.attr('href')
+          }
+          items.push(item);
+      });
+      //调用回调函数，传入错误对象和数组
+      callback(err,items);
+  })
+}
+var url = 'http://top.baidu.com/buzz?b=26&c=1&fr=topcategory_c1';
+module.exports(url,function(err,movies){
+    console.log(movies);
+});
